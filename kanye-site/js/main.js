af0779f2b7aaa9e9816ab1e5b1e726c6e5474de4
unitive-sam <sam@unitive.works>
2014-12-01 10:52:29 -0700

    kanye site now somewhat interesting

--- kanye-site/js/main.js
+++ kanye-site/js/main.js
@@ -4,15 +4,38 @@ var newValues = function (oldCoord) {
 
   for (var i = 0; i < charArray.length; i++) {
     var item = charArray[i];
-    //console.log(item + " : " + isNaN(item));
+    var nextKey = i + 1;
+    var nextItem = charArray[nextKey];
     var numCheck = parseInt(item);
 
-    if (!isNaN(numCheck) && (Math.random() > 0.5)) {
-      item = (item + (Math.random() * 1));
-      item = Math.ceil(item);
+    if (!isNaN(numCheck) && (Math.random() > 0.99)) {
+      if (nextItem === ',' || nextItem === '-' || nextItem === '.') {
+        item = (numCheck + (Math.random() * 1));
+        item = Math.ceil(item);
+        item = item.toString();
+      }
     }
     newArray.push(item);
   }
 
   return newArray.join('');
 };
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
